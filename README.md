a9793b2fc1722bdaac881f63c13847dec39f97a1
Logan Shy <loganshy740@gmail.com>
2020-05-30 18:19:59 -0600

    fixed API methods and added refresh time button

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Chonk Tracker
-This android application is a passion project as an excuse to explore mobile development, dynamic state changes, and persistent storage. The applications main purpose to is track and record the last time my cat was fed, since my roommate and I constantly have to ask each other if they have fed the cat. He can be quite the convincing actor at times. To build, open the application in Android Studio and use the gradle files to build. To install the app, simply download the app > debug > ChonkTracker.apk and install onto the desired android powered device. 
+This android application is a passion project as an excuse to explore mobile development, dynamic state changes, and persistent storage. The applications main purpose to is track and record the last time my cat was fed, since my roommate and I constantly have to ask each other if they have fed the cat. He can be quite the convincing actor at times. To build, open the application in Android Studio and use the gradle files to build. To install the app, simply download the app > release > ChonkTracker.apk and install onto the desired android powered device. 
 
 Chonk Tracker support versions of Android as far back as Jellybean, and should run on screen sizes from the razer 5 to the Pixel XL.
 
--- app/src/main/java/com/example/chonktracker/MainActivity.kt
+++ app/src/main/java/com/example/chonktracker/MainActivity.kt
@@ -1,21 +1,61 @@
 package com.example.chonktracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import org.json.JSONObject
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get Current time since fed
+        getTimeSinceFed(findViewById(R.id.textView2))
     }
 
-    fun feedQuant(view: View){
-        setContentView(R.layout.activity_main)
 
+
+    public fun getTimeSinceFed(view: View) {
+        // get content view and initialize networking
+        setContentView(R.layout.activity_main)
+        AndroidNetworking.initialize(getApplicationContext())
+        var timeSinceFed : String = "Default Text"
+
+        // Get Current time since fed from backend service
+        AndroidNetworking.get("http://157.245.250.195/")
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    timeSinceFed = response.get("time").toString()
+                    val timeView : TextView = findViewById(R.id.textView2)
+                    timeView.text = timeSinceFed
+                }
+
+                override fun onError(error: ANError) {
+                    timeSinceFed = error.message.toString()
+                    val timeView : TextView = findViewById(R.id.textView2)
+                    timeView.text = timeSinceFed
+                }
+            })
+
+    }
+
+
+
+    fun updateTime(view: View){
+        setContentView(R.layout.activity_main)
+        AndroidNetworking.initialize(getApplicationContext())
+
+        // Use java util calendar method to get current hour and minute
+        // Format differently depending on AM or PM and fill with 0's if less than 10
         val c = Calendar.getInstance()
         var minutes : String = c.get(Calendar.MINUTE).toString()
         if (minutes.toInt() < 10){
@@ -23,14 +63,38 @@ class MainActivity : AppCompatActivity() {
         }
         val hour = c.get(Calendar.HOUR)
         var pmOrAm : String = c.get(Calendar.AM_PM).toString()
-        if(pmOrAm == "1"){
-            pmOrAm = "P.M."
+        pmOrAm = if(pmOrAm == "1"){
+            "P.M."
         } else {
-            pmOrAm = "A.M."
+            "A.M."
         }
         val time = "$hour:$minutes $pmOrAm"
 
+        // Set time since fed to time var
         val timeView: TextView = findViewById(R.id.textView2)
         timeView.text = time
+
+        // Update the backend with the time in JSON format
+        val timeJSON = JSONObject();
+        timeJSON.put("time", time)
+        AndroidNetworking.post("http://157.245.250.195/")
+            .addJSONObjectBody(timeJSON)
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener{
+                override fun onResponse(response: JSONObject) {
+                    // No error, so post was successful
+                }
+
+                override fun onError(error: ANError) {
+//                    val timeSinceFed = error.message
+//                    val timeView: TextView = findViewById(R.id.textView2)
+//                    timeView.text = timeSinceFed
+                }
+            })
+
+    }
+
+    fun calculateHunger(hour : String, pmam : String){
+
     }
 }
