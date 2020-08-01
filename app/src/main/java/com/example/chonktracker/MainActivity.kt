ff0f81a7101ddb3a0ad62d799b666f4cfdef927c
Logan Shy <loganshy740@gmail.com>
2020-08-01 16:11:33 -0600

    added support for day of week to front and backend, and began implementation of schedule hunger worker

--- app/src/main/java/com/example/chonktracker/MainActivity.kt
+++ app/src/main/java/com/example/chonktracker/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.chonktracker
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.JSONObjectRequestListener
@@ -23,26 +26,28 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    public fun getTimeSinceFed(view: View) {
+    fun getTimeSinceFed(view: View) {
         // get content view and initialize networking
         setContentView(R.layout.activity_main)
         AndroidNetworking.initialize(getApplicationContext())
-        var timeSinceFed : String = "Default Text"
+        var timeSinceFed = "Default Time Text"
+        var dayFed = "Default Day Text"
 
-        // Get Current time since fed from backend service
+        // Get Current time and day since fed from backend service
         AndroidNetworking.get("http://157.245.250.195/")
             .build()
             .getAsJSONObject(object : JSONObjectRequestListener {
                 override fun onResponse(response: JSONObject) {
                     timeSinceFed = response.get("time").toString()
+                    dayFed = response.get("day").toString()
+
                     val timeView : TextView = findViewById(R.id.textView2)
-                    timeView.text = timeSinceFed
+                    timeView.text = "$dayFed, $timeSinceFed"
                 }
 
                 override fun onError(error: ANError) {
-                    timeSinceFed = error.message.toString()
                     val timeView : TextView = findViewById(R.id.textView2)
-                    timeView.text = timeSinceFed
+                    timeView.text = error.message.toString()
                 }
             })
 
@@ -57,6 +62,7 @@ class MainActivity : AppCompatActivity() {
         // Use java util calendar method to get current hour and minute
         // Format differently depending on AM or PM and fill with 0's if less than 10
         val c = Calendar.getInstance()
+        var day : String
         var minutes : String = c.get(Calendar.MINUTE).toString()
         if (minutes.toInt() < 10){
             minutes = "0$minutes"
@@ -68,15 +74,30 @@ class MainActivity : AppCompatActivity() {
         } else {
             "A.M."
         }
+
+        when(c.get(Calendar.DAY_OF_WEEK)){
+            Calendar.SUNDAY -> day = "Sunday"
+            Calendar.MONDAY -> day = "Monday"
+            Calendar.TUESDAY -> day = "Tuesday"
+            Calendar.WEDNESDAY -> day = "Wednesday"
+            Calendar.THURSDAY -> day = "Thursday"
+            Calendar.FRIDAY -> day = "Friday"
+            Calendar.SATURDAY -> day = "Saturday"
+            Calendar.SUNDAY -> day = "Sunday"
+            else -> day ="day N/A"
+        }
+
+
         val time = "$hour:$minutes $pmOrAm"
 
-        // Set time since fed to time var
+        // Set time since fed to time and day var
         val timeView: TextView = findViewById(R.id.textView2)
-        timeView.text = time
+        timeView.text = "$day, $time"
 
-        // Update the backend with the time in JSON format
+        // Update the backend with the time and day in JSON format
         val timeJSON = JSONObject();
         timeJSON.put("time", time)
+        timeJSON.put("day", day)
         AndroidNetworking.post("http://157.245.250.195/")
             .addJSONObjectBody(timeJSON)
             .build()
@@ -86,15 +107,53 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onError(error: ANError) {
-//                    val timeSinceFed = error.message
 //                    val timeView: TextView = findViewById(R.id.textView2)
-//                    timeView.text = timeSinceFed
+//                    timeView.text = error.message
                 }
             })
 
     }
 
-    fun calculateHunger(hour : String, pmam : String){
+    //TODO: schedule worker to calculate hunger since fed
+    fun updateHunger(view: View) {
+        // get content view and initialize networking
+        setContentView(R.layout.activity_main)
+        AndroidNetworking.initialize(getApplicationContext())
+        var timeSinceFed: String
 
+        // Get Current time since fed from backend service
+        AndroidNetworking.get("http://157.245.250.195/")
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    //get time and strip to first character
+                    timeSinceFed = response.get("time").toString()
+                    val timeView : TextView = findViewById(R.id.textView2)
+                    timeView.text = timeSinceFed
+                }
+
+                override fun onError(error: ANError) {
+                    timeSinceFed = error.message.toString()
+                    val timeView : TextView = findViewById(R.id.textView2)
+                    timeView.text = timeSinceFed
+                }
+            })
+
+    }
+
+}
+
+class DinnerBellWorker(appContext: Context, workerParams: WorkerParameters) :
+    Worker(appContext, workerParams) {
+
+    override fun doWork(): Result {
+        //Do the work here
+        val timeSinceFed =
+            inputData.getString("TIME_SINCE_FED") ?: return Result.failure()
+
+
+
+        //Indicate whether the work finished successfully with the Result
+        return Result.success()
     }
 }
